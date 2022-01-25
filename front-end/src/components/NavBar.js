7bcaba3fb1834a5691de971197a59c31fff38354
Eder Santos <3derms@gmail.com>
2022-01-25 18:35:55 -0300

    Crie uma tela de produtos do cliente contendo uma barra de navegação - navbar

---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NavBar () {
+  return (
+    <div>
+      <div>PRODUTOS</div>
+      <div>MEUS PEDIDOS</div>
+      <div>Cicrano da Silva</div>
+      <div>Sair</div>
+    </div>
+  )
+}
