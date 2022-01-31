3b01aa7e42f7ceba1a9a362ad926c3308608ab2c
Eder Santos <3derms@gmail.com>
2022-01-31 20:16:46 -0300

    Inclui links ao NavBar

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,28 +1,37 @@
 import React from 'react';
 
 export default function NavBar() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const clearStorage = () => localStorage.removeItem('user');
+
   return (
     <div>
-      <div
+      <Link
         data-testid="customer_products__element-navbar-link-products"
+        to="/products"
       >
         PRODUTOS
-      </div>
-      <div
+      </Link>
+      <Link
         data-testid="customer_products__element-navbar-link-orders"
+        to="/customer/orders"
       >
         MEUS PEDIDOS
-      </div>
-      <div
+      </Link>
+      <Link
         data-testid="customer_products__element-navbar-user-full-name"
+        to="users/:id"
       >
-        Cicrano da Silva
-      </div>
-      <div
+        {user ? user.name : null}
+      </Link>
+      <Link
         data-testid="customer_products__element-navbar-link-logout"
+        to="/login"
+        onClick={ () => clearStorage() }
       >
         Sair
-      </div>
+      </Link>
     </div>
   );
 }
