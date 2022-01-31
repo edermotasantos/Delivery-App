f280dc88bf016371792264784e56c62a60d0e058
Eder Santos <3derms@gmail.com>
2022-01-31 18:57:15 -0300

    Inclui data-testid na NavBar

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -3,10 +3,18 @@ import React from 'react';
 export default function NavBar() {
   return (
     <div>
-      <div>PRODUTOS</div>
-      <div>MEUS PEDIDOS</div>
-      <div>Cicrano da Silva</div>
-      <div>Sair</div>
+      <div
+        data-testid="customer_products__element-navbar-link-products"
+      >PRODUTOS</div>
+      <div
+        data-testid="customer_products__element-navbar-link-orders"
+      >MEUS PEDIDOS</div>
+      <div 
+        data-testid="customer_products__element-navbar-user-full-name"
+      >Cicrano da Silva</div>
+      <div
+        data-testid="customer_products__element-navbar-link-logout"
+      >Sair</div>
     </div>
   );
 }
